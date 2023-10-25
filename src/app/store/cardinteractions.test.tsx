5840e877e9097eefedfc71436d005a509b974856
Maksim <vl4dimirov@gmail.com>
2023-10-25 09:31:00 +0300

    add: add tests for minus, plus and remove elements

--- src/app/store/cardinteractions.test.tsx
+++ src/app/store/cardinteractions.test.tsx
@@ -11,6 +11,7 @@ describe('Cartstore тест', () => {
     count: 1,
     img: 'https://www.gastronom.ru/binfiles/images/20171102/b65d30ca.jpg',
   };
+
   test('Тест на добавление элемента', () => {
     expect(cartStore.items).toEqual([]);
     expect(cartStore.totalPrice).toEqual(0);
@@ -22,6 +23,32 @@ describe('Cartstore тест', () => {
       count: 1,
     });
 
-    expect(cartStore.totalPrice).toEqual(calcTotalPrice([itemToAdd]));
+    expect(cartStore.totalPrice).toEqual(calcTotalPrice([...cartStore.items]));
+  });
+
+  test('Тест на плюс количества элемента', () => {
+    cartStore.plusItem(itemToAdd);
+    expect(cartStore.items).toContainEqual({
+      ...itemToAdd,
+      count: 2,
+    });
+
+    expect(cartStore.totalPrice).toEqual(calcTotalPrice([...cartStore.items]));
+  });
+
+  test('Тест на минус количества элемента', () => {
+    cartStore.minusItem(itemToAdd);
+
+    expect(cartStore.items).toContainEqual({
+      ...itemToAdd,
+      count: 1,
+    });
+
+    expect(cartStore.totalPrice).toEqual(calcTotalPrice([...cartStore.items]));
+  });
+
+  test('Тест на очистку всех элементов', () => {
+    cartStore.clearItems();
+    expect(cartStore.items).toEqual([]);
   });
 });
